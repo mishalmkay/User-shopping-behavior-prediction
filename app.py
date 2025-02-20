bafd24c2bf9f7de22f5faea117c87f05be5c0ed4
mishalmkay <mishalmkay@gmail.com>
2025-02-20 14:05:33 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,7 @@ def index():
         return render_template('index.html', result= None)
 
 
-if __name__ == '__main__':
-    app.run(debug= True, port= 5001)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=False)
